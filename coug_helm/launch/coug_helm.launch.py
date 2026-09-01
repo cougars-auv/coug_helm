5ad9ce975a5bfde697af19917e7367b28ab9a65e
Nelson Durrant <snelsondurrant@gmail.com>
2026-09-01 10:56:44 -0600

    Better launch files

--- coug_helm/launch/coug_helm.launch.py
+++ coug_helm/launch/coug_helm.launch.py
@@ -27,14 +27,14 @@ def generate_launch_description() -> LaunchDescription:
     use_sim_time = LaunchConfiguration("use_sim_time")
     agent_ns = LaunchConfiguration("agent_ns")
 
-    fleet_params = PathJoinSubstitution(
+    fleet_param_file = PathJoinSubstitution(
         [
             EnvironmentVariable("CONFIG_DIR"),
             "fleet",
             "coug_helm_params.yaml",
         ]
     )
-    agent_params = PathJoinSubstitution(
+    agent_param_file = PathJoinSubstitution(
         [
             EnvironmentVariable("CONFIG_DIR"),
             PythonExpression(["'", agent_ns, "' + '_params.yaml'"]),
@@ -58,8 +58,8 @@ def generate_launch_description() -> LaunchDescription:
                 executable="bt_helm",
                 name="bt_helm_node",
                 parameters=[
-                    fleet_params,
-                    agent_params,
+                    fleet_param_file,
+                    agent_param_file,
                     {"use_sim_time": use_sim_time},
                 ],
             ),
