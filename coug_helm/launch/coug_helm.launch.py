5b144ea5d046c47773436f38ea0725f6f5818159
Nelson Durrant <snelsondurrant@gmail.com>
2026-09-24 15:14:18 -0600

    Better launch file param handling

--- coug_helm/launch/coug_helm.launch.py
+++ coug_helm/launch/coug_helm.launch.py
@@ -44,6 +44,7 @@ def launch_setup(context: LaunchContext, *args: Any, **kwargs: Any) -> list[Acti
     agent_ns = LaunchConfiguration("agent_ns")
 
     agent_ns_str = agent_ns.perform(context)
+    scenario_param_path = LaunchConfiguration("scenario_param_file").perform(context)
 
     config_dir = os.environ["CONFIG_DIR"]
     coug_helm_dir = get_package_share_directory("coug_helm")
@@ -54,17 +55,18 @@ def launch_setup(context: LaunchContext, *args: Any, **kwargs: Any) -> list[Acti
     agent_param_file = PathJoinSubstitution(
         [EnvironmentVariable("CONFIG_DIR"), [agent_ns, "_params.yaml"]]
     )
-    scenario_param_file = (
-        LaunchConfiguration("scenario_param_file").perform(context) or agent_param_file
-    )
+    scenario_param_file = scenario_param_path or agent_param_file
 
-    fleet_launch_params = load_launch_params(
-        os.path.join(config_dir, "fleet", "coug_helm_params.yaml"), "/**"
-    )
-    agent_launch_params = load_launch_params(
-        os.path.join(config_dir, f"{agent_ns_str}_params.yaml"), f"/{agent_ns_str}"
-    )
-    tree_filename = agent_launch_params.get("tree_file", fleet_launch_params.get("tree_file"))
+    fleet_param_path = os.path.join(config_dir, "fleet", "coug_helm_params.yaml")
+    agent_param_path = os.path.join(config_dir, f"{agent_ns_str}_params.yaml")
+
+    launch_params = {
+        **load_launch_params(fleet_param_path, "/**"),
+        **load_launch_params(agent_param_path, f"/{agent_ns_str}"),
+        **load_launch_params(scenario_param_path, "/**"),
+        **load_launch_params(scenario_param_path, f"/{agent_ns_str}"),
+    }
+    tree_filename = launch_params["tree_file"]
     tree_file = os.path.join(coug_helm_dir, "trees", tree_filename)
 
     with open(tree_file) as tree:
